59aa4f8ed6b3dc8cd71fdbde2a52ffab5b9a64e7
Naji Achkar <hquart@icloud.com>
2019-12-16 18:02:38 +0100

    OK 16/12/19

--- instagrid/Controller/ViewController.swift
+++ instagrid/Controller/ViewController.swift
@@ -8,29 +8,61 @@
 
 import UIKit
 
-
-
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     let imagePicker = UIImagePickerController()
     var currentButton : UIButton?
     
     override func viewDidLoad() {
-        // par défaut layout de gauche selectionné
         super.viewDidLoad()
         imagePicker.delegate = self
-        // Adding gesture recognizer to the Main View
-        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe(_:)))
-        swipeUp.direction = UISwipeGestureRecognizer.Direction.up
-        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe(_:)))
-        swipeUp.direction = UISwipeGestureRecognizer.Direction.left
-        mainView.addGestureRecognizer(swipeUp)
-        mainView.addGestureRecognizer(swipeLeft)
+        //Left layout is selected by default when the app is launched
+        changeLayout(layoutButtons[0])
     }
     
-
+    @IBAction func handleSwipe(_ sender: UISwipeGestureRecognizer?) {
+        if let gesture = sender {
+            if UIDevice.current.orientation.isPortrait && gesture.direction == .up {
+                print("I SWIPPPED UP")
+                moveMainViewUp()
+            
+            } else if UIDevice.current.orientation.isLandscape && gesture.direction == .left {
+                print("I SWIPPED LEFT")
+                moveMainViewLeft()
+            }
+        }
+    }
     
+    func moveMainViewUp() {
+        UIView.animate(withDuration: 2, animations: {
+            self.mainView.transform = CGAffineTransform(translationX: 0, y: -UIScreen.main.bounds.height)
+        }, completion: {
+            (true) in
+            self.shareLayout()
+        })
+    }
     
+    func moveMainViewLeft() {
+        UIView.animate(withDuration: 2, animations: {
+            self.mainView.transform = CGAffineTransform(translationX: -UIScreen.main.bounds.width, y: 0)
+        }, completion: {
+            (true) in
+            self.shareLayout()
+        })
+        
+    }
+    
+//    @objc func swipeAction(_ sender: UISwipeGestureRecognizer) {
+//        if UIDevice.current.orientation.isPortrait && sender.direction == .up {
+//          self.mainView.transform = CGAffineTransform(translationX: 0, y: UIScreen.main.bounds.height)
+//            print("I SWIPPPED UP")
+//            shareLayout()
+//        } else if UIDevice.current.orientation.isLandscape && sender.direction == .left {
+//            mainView.transform = CGAffineTransform(translationX: UIScreen.main.bounds.width, y: 0)
+//            print("I SWIPPED LEFT")
+//            shareLayout()
+//        }
+//    }
     // This method will launch the imagePicker when a button is tapped
     @IBAction func loadImageButtonTapped(_ sender: UIButton) {
         self.currentButton = sender
@@ -39,118 +71,69 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         imagePicker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
         present(imagePicker, animated: true, completion: nil)
     }
-    
     // I create an Outlet collection to identify each photo button
     @IBOutlet var photoButtons: [UIButton]!
-    
     // I create an Outlet collection to identify each layout button
     @IBOutlet var layoutButtons: [UIButton]!
-    
-    //OUTLET of the Main View which will be shared
+    // I create an OUtlet of the mainView so I can share it
     @IBOutlet weak var mainView: UIView!
-  
-    func moveMainViewUp() {
-          
-       }
-    //CRéer 2 methodes une qui move up et l'autre back in
-    @objc func handleSwipe(_ gesture: UISwipeGestureRecognizer) {
-        if UIDevice.current.orientation.isPortrait && gesture.direction == UISwipeGestureRecognizer.Direction.up {
-            mainView.transform = CGAffineTransform(translationX: 0, y: UIScreen.main.bounds.height)
-            shareLayout()
-        } else if UIDevice.current.orientation.isLandscape && gesture.direction == UISwipeGestureRecognizer.Direction.left {
-            mainView.transform = CGAffineTransform(translationX: UIScreen.main.bounds.width, y: 0)
-            shareLayout()
-    }
-    }
+    // I create an Outlet of the Swipe Text Label to init the swipe gesture from it
+    @IBOutlet weak var swipeLabel: UILabel!
     
-   func shareLayout() {
-        let content = mainView.asImage()
-        let vc = UIActivityViewController(activityItems: [content], applicationActivities: nil)
-        present(vc, animated: true, completion: nil)
-    }
+    //Créer 2 methodes une qui move up et l'autre back in
     
-  
-    
-//    private func transformMainView(gesture: UISwipeGestureRecognizer) {
-//        let translation = gesture.translation(in: mainView)
-//        mainView.transform = CGAffineTransform(translationX: translation.x, y: translation.y)
-//    }
-    
-    // I create a function which removes pictures from buttons to have a clean layout when I switch
-    func cleanLayout() {
+    //This Method will make all photo buttons visible:
+    func showButtons() {
         for button in photoButtons {
-            button.setImage(nil, for: UIControl.State.normal)
+            button.isHidden = false
+        }
+    }
+    func shareLayout() {
+        let content = mainView.asImage()
+        let activityController = UIActivityViewController(activityItems: [content], applicationActivities: nil)
+        self.present(activityController, animated: true, completion: nil)
+        // We use the completion handler to move back the mainView when the activityController is closed
+        activityController.completionWithItemsHandler = {  (activity, success, items, error) in
+            UIView.animate(withDuration: 1, animations: {
+                self.mainView.transform = .identity
+            }, completion: nil)
         }
     }
         
-    //creer une methode generique pour tous les layout avec parametre boutton avec un switch
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    @IBAction func leftLayout(_ sender: Any) {
-        cleanLayout()
-        layoutButtons[0].setBackgroundImage(UIImage(named: "Selected"), for: UIControl.State.normal)
-        layoutButtons[1].setBackgroundImage(UIImage(named: "unselect2"), for: UIControl.State.normal)
-        layoutButtons[2].setBackgroundImage(UIImage(named: "unselect3"), for: UIControl.State.normal)
-        photoButtons[0].isHidden = true
-        photoButtons[1].isHidden = false
-        photoButtons[1].contentMode = UIView.ContentMode.scaleToFill
-        photoButtons[2].isHidden = false
-        photoButtons[3].isHidden = false
+    // This method will implement chosen layout on the mainView
+    @IBAction func changeLayout(_ sender: UIButton) {
+        sender.setBackgroundImage(UIImage(named: "Selected"), for: UIControl.State.normal)
+        switch sender {
+        case layoutButtons[0]:
+            layoutButtons[1].setBackgroundImage(UIImage(named: "unselect2"), for: UIControl.State.normal)
+            layoutButtons[2].setBackgroundImage(UIImage(named: "unselect3"), for: UIControl.State.normal)
+            showButtons()
+            photoButtons[0].isHidden = true
+        case layoutButtons[1]:
+            layoutButtons[0].setBackgroundImage(UIImage(named: "unselect1"), for: UIControl.State.normal)
+            layoutButtons[2].setBackgroundImage(UIImage(named: "unselect3"), for: UIControl.State.normal)
+            showButtons()
+            photoButtons[2].isHidden = true
+        case layoutButtons[2]:
+            layoutButtons[0].setBackgroundImage(UIImage(named: "unselect1"), for: UIControl.State.normal)
+            layoutButtons[1].setBackgroundImage(UIImage(named: "unselect2"), for: UIControl.State.normal)
+            showButtons()
+        default: break
+        }
     }
-    
-    @IBAction func middleLayout(_ sender: Any) {
-        cleanLayout()
-        layoutButtons[0].setBackgroundImage(UIImage(named: "unselect1"), for: UIControl.State.normal)
-        layoutButtons[1].setBackgroundImage(UIImage(named: "Selected"), for: UIControl.State.normal)
-        layoutButtons[2].setBackgroundImage(UIImage(named: "unselect3"), for: UIControl.State.normal)
-        photoButtons[0].isHidden = false
-        photoButtons[1].isHidden = false
-        photoButtons[2].isHidden = true
-        photoButtons[3].isHidden = false
-        photoButtons[3].contentMode = UIView.ContentMode.scaleToFill
-    }
-    
-    @IBAction func rightLayout(_ sender: Any) {
-        cleanLayout()
-        layoutButtons[0].setBackgroundImage(UIImage(named: "unselect1"), for: UIControl.State.normal)
-        layoutButtons[1].setBackgroundImage(UIImage(named: "unselect2"), for: UIControl.State.normal)
-        layoutButtons[2].setBackgroundImage(UIImage(named: "Selected"), for: UIControl.State.normal)
-        photoButtons[0].isHidden = false
-        photoButtons[1].isHidden = false
-        photoButtons[2].isHidden = false
-        photoButtons[3].isHidden = false
-    }
-    
     // METHOD FOR THE DELEGATE: when the user picks up an image from the photo library, image is set to the button
-     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-         if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             self.currentButton?.setImage(pickedImage, for: UIControl.State.normal)
-         }
-         dismiss(animated: true, completion: nil)
-     }
-    
-       // METHOD FOR THE DELEGATE: when the user cancels
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    // METHOD FOR THE DELEGATE: when the user cancels image picking
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-             dismiss(animated: true, completion: nil)
+        dismiss(animated: true, completion: nil)
     }
-    
-    
-    
-    
-    
-    
-
-    }
-
-
-// This UIView extension will permit to convert our MainView to an image
+}
+// This UIView extension will permit to convert our MainView to an image file
 extension UIView {
     func asImage() -> UIImage {
         let renderer = UIGraphicsImageRenderer(bounds: bounds)
@@ -159,15 +142,15 @@ extension UIView {
         }
     }
 }
-    
 
-    
-    
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
+
+
+
 
 
